9304ae9a030b63c913a35d505e4430c024869641
Laura Kovačić <laurakciic@gmail.com>
2022-04-28 20:28:35 +0200

    add test sound

--- src/subComponents/SoundBar.js
+++ src/subComponents/SoundBar.js
@@ -10,6 +10,44 @@ position: fixed;
 left: 8rem;
 top: 3rem;
 z-index: 10;
+
+&>*:nth-child(1){
+    animation-delay: 0.2s;
+}
+&>*:nth-child(2){
+    animation-delay: 0.3s;
+}
+&>*:nth-child(3){
+    animation-delay: 0.4s;
+}
+&>*:nth-child(4){
+    animation-delay: 0.5s;
+}
+&>*:nth-child(5){
+    animation-delay: 0.8s;
+}
+`
+
+const play = keyframes `
+0%{
+    transform: scaleY(1);
+}
+50%{
+    transform: scaleY(2);
+}
+100%{
+    transform: scaleY(1);
+}
+`
+const Line = styled.span`
+background: ${props => props.theme.text};
+border: 1px solid ${props => props.theme.body};
+
+animation: ${play} 1s ease infinite;
+animation-play-state: ${props => props.click ? "running" : "paused"};
+height: 1rem;
+width: 2px;
+margin: 0 0.1rem;
 `
 
 const SoundBar = () => {
@@ -29,7 +67,11 @@ const SoundBar = () => {
       
   return (
     <Box onClick={() => handleClick()}>
-        Sound
+        <Line click={click}/>
+        <Line click={click}/>
+        <Line click={click}/>
+        <Line click={click}/>
+        <Line click={click}/>
 
         <audio src={music} ref={ref} loop />
     </Box>
