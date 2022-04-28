5312ab7ff6ef4577442e724a61ea405a9af833f1
Laura Kovačić <laurakciic@gmail.com>
2022-04-28 16:30:43 +0200

    add description layout in skills section

--- src/components/OurSkillsPage.js
+++ src/components/OurSkillsPage.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 import { ThemeProvider } from 'styled-components'
+import LogoComponent from '../subComponents/LogoComponent'
+import SocialIcons from '../subComponents/SocialIcons'
 import { lightTheme } from './Themes'
-
-
-
+import PowerButton from '../subComponents/PowerButton'
 
 const Box = styled.div`
 background-color: ${props => props.theme.body};
@@ -24,18 +24,110 @@ width: 30vw;
 height: 60vh;
 z-index: 3;
 line-height: 1.5;
+
+font-family: 'Ubunto Mono', monospace;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+
+&:hover{
+  color: ${props => props.theme.body};
+  background-color: ${props => props.theme.text};
+}
+`
+
+const Title = styled.h2`
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: calc(1em + 1vw);
+
+${Main}:hover &{
+  &>*{
+    fill:${props => props.theme.body};
+  }
+}
+
+&>*:first-child{
+  margin-right: 1rem;
+}
+`
+
+const Description = styled.div`
+color: ${props => props.theme.text};
+font-size: calc(0.6em + 1vw);
+padding: 0.5rem 0;
+
+${Main}:hover &{
+  color:${props => props.theme.body};
+}
+
+strong{
+  margin-bottom: 1rem;
+  text-tranform: uppercase;
+}
+ul,p{
+  margin-left: 2rem;
+}
 `
 
 const OurSkillsPage = () => {
   return (
     <ThemeProvider theme={lightTheme}>
       <Box>
+        <LogoComponent theme='light'/>
+        <SocialIcons theme='light'/>
+        <PowerButton />
+
         <Main>
-        1
+          <Title>
+            Laura
+          </Title>
+          <Description>
+            laurin opis
+          </Description>
+          <Description>
+            <strong> wiiii </strong>
+            <ul>
+              <li>
+                1
+              </li>
+              <li>
+                2
+              </li>
+            </ul>
+          </Description>
+          <Description>
+            <strong> tools </strong>
+            <ul>
+              <li>
+                11111111
+              </li>
+            </ul>
+          </Description>
         </Main>
+
         <Main>
-        2
+          <Title>
+            Mirjam
+          </Title>
+          <Description>
+            mirjamin opis
+          </Description>
+          <Description>
+            <strong> wiiii2 </strong>
+            <p>
+              1, 2, 3, 4, 5
+            </p>
+          </Description>
+          <Description>
+            <strong> tools </strong>
+            <p>
+              tttttttttt
+            </p>
+          </Description>
         </Main>
+
       </Box>
     </ThemeProvider>
     
